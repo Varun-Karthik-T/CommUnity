e3cda50ec990212f04ea358846e2fd4f11e2e87b
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2024-07-25 16:28:00 +0530

    eas and theme init

---
+++ App/Components/Hone/index.jsx
@@ -0,0 +1,16 @@
+import { View } from "react-native"
+import { Text, Button } from "react-native-paper"
+
+function Home() {
+  return (
+    <>
+    <View>
+        <Text>Home sweet home</Text>
+        <Button mode="outlined"> Don't stop me now </Button>
+    </View>
+      
+    </>
+  )
+}
+
+export default Home
--- App/app/index.jsx
+++ App/app/index.jsx
@@ -1,19 +1,56 @@
-import * as React from "react";
-import { Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import { PaperProvider } from "react-native-paper";
+import Home from "@/Components/Hone";
 
 export default function Main() {
+  const theme = {
+    colors: {
+      primary: "rgb(155, 68, 39)",
+      onPrimary: "rgb(255, 255, 255)",
+      primaryContainer: "rgb(255, 219, 208)",
+      onPrimaryContainer: "rgb(58, 11, 0)",
+      secondary: "rgb(119, 87, 77)",
+      onSecondary: "rgb(255, 255, 255)",
+      secondaryContainer: "rgb(255, 219, 208)",
+      onSecondaryContainer: "rgb(44, 22, 14)",
+      tertiary: "rgb(107, 94, 47)",
+      onTertiary: "rgb(255, 255, 255)",
+      tertiaryContainer: "rgb(244, 226, 167)",
+      onTertiaryContainer: "rgb(34, 27, 0)",
+      error: "rgb(186, 26, 26)",
+      onError: "rgb(255, 255, 255)",
+      errorContainer: "rgb(255, 218, 214)",
+      onErrorContainer: "rgb(65, 0, 2)",
+      background: "rgb(255, 251, 255)",
+      onBackground: "rgb(32, 26, 24)",
+      surface: "rgb(255, 251, 255)",
+      onSurface: "rgb(32, 26, 24)",
+      surfaceVariant: "rgb(245, 222, 215)",
+      onSurfaceVariant: "rgb(83, 67, 63)",
+      outline: "rgb(133, 115, 110)",
+      outlineVariant: "rgb(216, 194, 188)",
+      shadow: "rgb(0, 0, 0)",
+      scrim: "rgb(0, 0, 0)",
+      inverseSurface: "rgb(54, 47, 45)",
+      inverseOnSurface: "rgb(251, 238, 234)",
+      inversePrimary: "rgb(255, 181, 158)",
+      elevation: {
+        level0: "transparent",
+        level1: "rgb(250, 242, 244)",
+        level2: "rgb(247, 236, 238)",
+        level3: "rgb(244, 231, 231)",
+        level4: "rgb(243, 229, 229)",
+        level5: "rgb(241, 225, 225)",
+      },
+      surfaceDisabled: "rgba(32, 26, 24, 0.12)",
+      onSurfaceDisabled: "rgba(32, 26, 24, 0.38)",
+      backdrop: "rgba(59, 45, 41, 0.4)",
+    },
+  };
   return (
     <>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Button
-          icon="camera"
-          mode="contained"
-          onPress={() => console.log("Pressed")}
-        >
-          Press me
-        </Button>
-      </View>
+      <PaperProvider theme={theme}>
+        <Home />
+      </PaperProvider>
     </>
   );
 }
