90c1b336bf3d6b52651e8438e751f18879a2c99c
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-08-01 18:47:14 +0530

    ..

--- App/Components/Home/SHGBookkeeping/index.jsx
+++ App/Components/Home/SHGBookkeeping/index.jsx
@@ -1,9 +1,25 @@
-import {Text} from 'react-native';
+import { Text } from "react-native";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function SHGBookkeeping() {
-    return (
-        <>
-            <Text>SHG Bookkeeping123</Text>
-        </>
-    )
+  const [shgData, setShgData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let response = await axios.get("http://10.11.53.8:5000/fetchSHG");
+        setShgData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Text>SHG Bookkeeping123</Text>
+    </>
+  );
 }
--- App/Components/SHGProfile/index.jsx
+++ App/Components/SHGProfile/index.jsx
@@ -1,13 +1,29 @@
-import {Text} from 'react-native';
-import Form from '../Home/Form';
+import { Text,View } from "react-native";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Form from "../Home/Form";
 
 export default function SHGProfile() {
-
-    return (
-        <>
-            <Text>SHG Profile</Text>
-           
-        </>
-    )
-
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let response = await axios.get("http://10.11.53.8:5000/fetchSHG");
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
+        <View>
+            <Text>{data[0].SHG_Name}</Text>
+            <Text>{data[0].SHG_ID}</Text>
+        </View>
+      
+    </>
+  );
 }
