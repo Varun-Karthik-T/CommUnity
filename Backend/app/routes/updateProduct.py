43f7f8450ad4ef05e12b14eba47d8b5ac232f866
Ezhil Dhiraviya <115528036+EzhilDhiraviya@users.noreply.github.com>
2024-08-21 19:38:34 +0530

    updated product.py

--- Backend/app/routes/updateProduct.py
+++ Backend/app/routes/updateProduct.py
@@ -5,22 +5,35 @@ from .. import db as d
 @app.post('/updateProduct')
 def updateProduct():
     try:
-
+        collection = d.db['products']
         data = request.json
-        
+
+        # existing_data = 
         if 'shg_id' not in data or 'price' not in data:
             return jsonify({'error': 'Missing required fields'}), 400
 
         shg_id = data['shg_id']
         product_name=data['product_name']
         new_price = data['price']
+        new_availability = data['availability']
+        image = data['image']
 
+        cursor = collection.find({'shg_id': shg_id, 'product_name': product_name})
+        if cursor.count() == 0:
+            collection.insert_one({ 
+                'shg_id': shg_id,
+                'product_name': product_name,
+                'price': new_price,
+                'availability': new_availability,
+                'image':image,
+                'quantity_sold':0
+            })
         try:
             new_price = float(new_price)
         except ValueError:
             return jsonify({'error': 'Invalid price value'}), 400
 
-        collection = d.db['products']
+        
 
         result = collection.update_one(
             {'shg_id': shg_id},
