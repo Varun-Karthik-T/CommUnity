858a36be71d35b99fdcd36f2c61a0e009d82edc0
Ezhil Dhiraviya <115528036+EzhilDhiraviya@users.noreply.github.com>
2024-08-21 17:23:59 +0530

    added addProduct.py

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -4,6 +4,6 @@ from flask_cors import CORS
 app=Flask(__name__)
 CORS(app)
 
-from .routes import hello, fetchSHGs, fetchMembers, fetchExpenses, fetchAllShg, fetchAllMembers, applyLoan
+from .routes import hello, fetchSHGs, fetchMembers, fetchExpenses, fetchAllShg, fetchAllMembers, applyLoan, addProduct
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
