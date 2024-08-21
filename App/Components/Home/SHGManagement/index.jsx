7c617bbdf8d1808b0c135e3dbe4b07bd311e9453
Ezhil Dhiraviya <115528036+EzhilDhiraviya@users.noreply.github.com>
2024-08-21 08:58:55 +0530

    updated SHGmanagement

--- App/Components/Home/SHGManagement/index.jsx
+++ App/Components/Home/SHGManagement/index.jsx
@@ -1,13 +1,35 @@
-import { Text } from 'react-native';
+import * as React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SegmentedButtons } from 'react-native-paper';
 
-function SHGManagement() {
+const SHGManagement = () => {
+  const [value, setValue] = React.useState('Attendance');
+
   return (
-    <Text>
-      Management
-            -atttendance
-            -finance
-    </Text>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <SegmentedButtons
+        value={value}
+        onValueChange={setValue}
+        buttons={[
+          {
+            value: 'Attendance',
+            label: 'Attendance',
+          },
+          {
+            value: 'Records',
+            label: 'Records',
+          },
+        ]}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
 
 export default SHGManagement;
