4e6565deee32c85891f56b383c8ee6760af91721
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2024-08-21 17:37:56 +0530

    performance done uh

--- Backend/app/routes/performance.py
+++ Backend/app/routes/performance.py
@@ -5,37 +5,33 @@ import xgboost as xgb
 import pandas as pd
 
 model = xgb.XGBRegressor()
-model.load_model('model/xgb_model.json')
+model.load_model('Model/xgb_model.json')
 print("Model loaded successfully")
 
 @app.get('/performance/<id>')
-def predict():
+def predict(id):
     collection = db.db['SHGs']
+    data = collection.find_one({"shg_id": id})
+    
+    if data:
+        initial_capital = data.get('initial_capital')
+        gross_profit = data.get('total_revenue')
+        profit_margin = (gross_profit / initial_capital) * 100
+        attendance = data.get('attendance')
+        loan_repayments = data.get('loan_repayment')
 
-    # Fetch all documents from the collection
-    data = collection.find( { "shg_id": id } )
-    print(data)
-    
-    return jsonify(data)
-    
-    # # Extract the required fields from the input JSON
-    # initial_capital = data.get('initial_capital')
-    # total_revenue = data.get('total_revenue')
-    # gross_profit = total_revenue - initial_capital
-    # profit_margins = (gross_profit / total_revenue) * 100
-    # attendance = data.get('attendance')
-    # loan_repayments = data.get('loan_repayments')
-    
-    # # Prepare the input data in the correct format
-    # user_data = pd.DataFrame({
-    #     'initial_capital': [initial_capital],
-    #     'gross_profit': [gross_profit],
-    #     'profit_margins': [profit_margins],
-    #     'attendance': [attendance],
-    #     'loan_repayments': [loan_repayments]
-    # })
+        user_data = pd.DataFrame({
+            'initial_capital': [initial_capital],
+            'gross_profit': [gross_profit],
+            'profit_margins': [profit_margin],
+            'attendance': [attendance],
+            'loan_repayments': [loan_repayments]
+        })
+        
+        print(user_data)
 
-    # # Predict the performance score
-    # predicted_score = model.predict(user_data)
-    
-    # return jsonify({'prediction': predicted_score.tolist()})
+        predicted_score = model.predict(user_data)
+        
+        return jsonify({'prediction': predicted_score.tolist()})
+    else:
+        return jsonify({"error": "Data not found"}), 404
