a0d87aeec27a6d8434285f448e13e43468c2f1e2
Ezhil Dhiraviya <115528036+EzhilDhiraviya@users.noreply.github.com>
2024-08-21 20:43:24 +0530

    added applyLoan.py

--- Backend/app/routes/applyLoan.py
+++ Backend/app/routes/applyLoan.py
@@ -1,13 +1,35 @@
 from flask import request
 from .. import app
 from .. import db as d
+import datetime
 
 @app.post('/applyLoan')
 def applyLoan():
     data = request.json
 
-    collection = d.db['loans']
+    if not all(key in data for key in ('member_id', 'purpose', 'loan_amount')):
+        return {'error': 'Missing required fields'}, 400
+    try:
 
-    collection.insert_one(data)
+        member_id = data['member_id']
+        purpose = data['purpose']
+        loan_amount = data['loan_amount']
+        date = datetime.datetime.now()
+        repayment_status = 'pending'
 
-    return {'success': True}
+        collection = d.db['loans']
+
+        loan_data = {
+            'member_id': member_id,
+            'purpose': purpose,
+            'loan_amount': loan_amount,
+            'date': date,
+            'repayment_status': repayment_status
+        }
+
+        collection.insert_one(loan_data)
+
+        return {'success': True}
+    except Exception as e:
+        return {'error': str(e)}, 500
+    
