3ad8d89a3d9b7901010f98f2d41231495cb37297
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2024-08-21 19:07:24 +0530

    pushhh

--- App/app/Performance/[id].jsx
+++ App/app/Performance/[id].jsx
@@ -15,7 +15,8 @@ function Performance() {
   const [expanded, setExpanded] = useState(false);
   const [showGraph, setShowGraph] = useState(false);
   const [shgData, setShgData] = useState();
-  const [expenses, setExpenses] = useState();
+  const [label, setLabel] = useState([]);
+  const [performance, setPerformance] = useState([]);
 
   const handlePress = () => setExpanded(!expanded);
   const toggleGraph = () => setShowGraph(!showGraph);
@@ -24,15 +25,46 @@ function Performance() {
       const response = await api.get(`/fetchSHGs/${id}`);
       setShgData(response.data);
       const expenses = await api.get(`/fetchExpenses/${id}`);
-      console.log(expenses.data);
-      setExpenses(expenses.data[0].records);
+      const performanceArray = Object.keys(expenses.data[0].records).map(
+        (date) => ({
+          date: date,
+          performance: expenses.data[0].records[date].performance,
+        })
+      );
+
+      const sortedPerformanceArray = performanceArray.sort(
+        (a, b) =>
+          new Date(a.date.split("/").reverse().join("-")) -
+          new Date(b.date.split("/").reverse().join("-"))
+      );
+
+      const newLabels = sortedPerformanceArray.map(
+        (performance) => performance.date
+      );
+      const newPerformances = sortedPerformanceArray.map(
+        (performance) => performance.performance
+      );
+      setLabel(newLabels);
+      setPerformance(newPerformances);
     } catch (error) {
       console.error(error);
     }
   };
+
   useEffect(() => {
     fetchData();
   }, []);
+
+  const data = {
+    labels: label,
+    datasets: [
+      {
+        data: performance,
+      },
+    ],
+    yAxisMin: 0,
+    yAxisMax: 100,
+  };
 
   const chartConfig = {
     backgroundColor: theme.colors.surface,
@@ -43,6 +75,9 @@ function Performance() {
     labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
     style: {
       borderRadius: 16,
+      paddingRight: 20,
+      paddingLeft: 20,
+      marginBottom: 20,
     },
     propsForBackgroundLines: {
       strokeWidth: 1,
@@ -50,27 +85,6 @@ function Performance() {
       strokeDasharray: "0",
     },
   };
-  // const data = {
-  //   labels: [
-  //     "Jan",
-  //     "Feb",
-  //     "Mar",
-  //     "Apr",
-  //     "May",
-  //     "Jun",
-  //     "Jul",
-  //     "Aug",
-  //     "Sep",
-  //     "Oct",
-  //     "Nov",
-  //     "Dec",
-  //   ],
-  //   datasets: [
-  //     {
-  //       data: profitData ? Object.values(profitData[0]) : [],
-  //     },
-  //   ],
-  // };
 
   return (
     <>
@@ -113,24 +127,18 @@ function Performance() {
               </Button>
             </Surface>
 
-            {/* <View style={{ marginBottom: 50 }}>
-          {showGraph && profitData && (
-            <>
-              <View style={{ display: "flex", flexDirection: "row" }}>
-                <Text style={{ fontWeight: "bold", fontSize: 20 }}>
-                  Risk Assessment Score:
-                </Text>
-                <Text style={{ fontSize: 20 }}>{shgData[0].Score}</Text>
-              </View>
-              <LineChart
-                data={data}
-                width={screenWidth}
-                height={220}
-                chartConfig={chartConfig}
-              />
-            </>
-          )}
-        </View> */}
+            <View style={{ marginBottom: 100 }}>
+              {showGraph && performance.length > 0 && (
+                <LineChart
+                  data={data}
+                  width={screenWidth}
+                  height={300}
+                  chartConfig={chartConfig}
+                  fromZero={true}
+                  verticalLabelRotation={-45}
+                />
+              )}
+            </View>
           </>
         )}
       </ScrollView>
@@ -171,12 +179,3 @@ const styles = StyleSheet.create({
     gap: 10,
   },
 });
-
-const dummy = {
-  title: "UPTOWN GIRLS",
-  origin: "Pudukkottai",
-  description:
-    "In the serene town of Pudukkottai, a story of resilience and hope began with five widows who dared to dream beyond their circumstances. These remarkable women came together to form the Self-Help Group (SHG) known as Uptown Girls. With the small capital they received, they started crafting delicious laddoos, infusing them with love and tradition. Their laddoos quickly became beloved in the local community, and the group found not just financial stability but a renewed sense of purpose. Through the platform provided by our app, Uptown Girls transformed their humble beginnings into a thriving business. They expanded their reach, utilized the extra capital to grow, and upskilled themselves along the way. Our app became a bridge to new opportunities, empowering them to take control of their future. Today, Uptown Girls is not just a symbol of delicious sweets but a testament to the strength and potential of women when given a chance. They are an inspiration to all, showing that with courage and unity, even the smallest beginnings can lead to great success.",
-  imageUri:
-    "https://th.bing.com/th/id/R.3cef5299a9fb84b64f846b564f4e895e?rik=JU9vcW0%2bi%2brRMw&riu=http%3a%2f%2fe3az4yc7762.exactdn.com%2fwp-content%2fuploads%2f2022%2f08%2fPCI-India_Womens-SHG.jpg&ehk=1%2fYxsWbcVnPpHDw1YTul18O2zgunEC1POyr7oWvHmpM%3d&risl=&pid=ImgRaw&r=0",
-};
